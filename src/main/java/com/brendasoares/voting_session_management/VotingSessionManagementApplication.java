746b11b8964c1eb0f023e9803d5ceeb460fce8c5
BrendaSoares <contatobrendadiniz.s@gmail.com>
2021-12-14 18:40:46 -0300

    init project

---
+++ src/main/java/com/brendasoares/voting_session_management/VotingSessionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.brendasoares.voting_session_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionManagementApplication.class, args);
+	}
+
+}
